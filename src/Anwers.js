338aed4caded0d7723cc436252b519953bca6ea6
juhavar <juhvart@gmail.com>
2020-11-10 22:46:00 +0200

    +

---
+++ src/Anwers.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Checkbox from '@material-ui/core/Checkbox'
+
+function Questions(props){
+
+    return (<div>
+      {props.answers.map((i, answerIndex) =>
+        <div key={answerIndex}>
+          <label className="checkbox">
+            <input type="checkbox" onChange={(e) =>
+              { props.selectedAnswer(e, props.index, props.examIndex, answerIndex) }}
+                 checked={i.checked} /><span>{i.a}</span></label></div>)}
+    </div>
+    
+    );
+    
+    }
+    export default Questions;
